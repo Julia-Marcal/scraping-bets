15a2604e65c786026d909183cf943f21b5ce73b6
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-05-12 15:30:15 -0300

    acessing allowed_games on the file

---
+++ src/ScrapingScripts/AllowedGames.ts
@@ -0,0 +1,5 @@
+import { browser } from './enterAccount';
+
+export async function ScrapingAllowedGames(allowed_games:  string[]) {
+  console.log(allowed_games)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { bestLeagues } from './ScrapingScripts/Leagues'
 import { enterAccount } from './ScrapingScripts/enterAccount'
 import { bestGames } from './ScrapingScripts/AllGames'
+import { ScrapingAllowedGames } from './ScrapingScripts/AllowedGames'
 
 const allowed_leagues: string[] = [
   'Brasileiro Série A',
@@ -16,11 +17,14 @@ const allowed_leagues: string[] = [
   'UEFA Europa Conference League'
 ]
 
+let allowed_games = [];
+
 async function run() {
   await enterAccount()
   await Promise.all([
     await bestLeagues(allowed_leagues),
-    await bestGames(allowed_leagues)
+    allowed_games = await bestGames(allowed_leagues),
+    await ScrapingAllowedGames(allowed_games)
   ])
 }
 run()
